06ce60d29b935cfad1339019c2014e8a82553cd5
epaulino27 <143731967+epaulino27@users.noreply.github.com>
2026-08-20 11:24:06 -0400

    Delete Inventory directory

--- Inventory/inv_item.gd
+++
@@ -1,8 +0,0 @@
-extends Resource
-
-class_name InvItem
-
-#this class is used as a inventory item interface
-@export var name: String = ""
-@export var texture: Texture2D
-@export var category: String = ""
--- Inventory/inv_slot.gd
+++
@@ -1,7 +0,0 @@
-extends Resource
-
-class_name InvSlot
-
-#used to hold an item which has a name and sprite
-@export var item: InvItem
-@export var amount: int
